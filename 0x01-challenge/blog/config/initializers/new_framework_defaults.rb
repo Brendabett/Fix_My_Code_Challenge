5ef660eb6d98961f60c0fde22654531e1c4084a6
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-10-03 10:05:01 +0300

    My commit

--- 0x01-challenge/blog/config/initializers/new_framework_defaults.rb
+++ 0x01-challenge/blog/config/initializers/new_framework_defaults.rb
@@ -14,7 +14,7 @@ Rails.application.config.action_controller.forgery_protection_origin_check = tru
 # Previous versions had false.
 ActiveSupport.to_time_preserves_timezone = true
 
-# Require  associations by default. Previous versions had false.
+# Require `belongs_to` associations by default. Previous versions had false.
 Rails.application.config.active_record.belongs_to_required_by_default = true
 
 # Do not halt callback chains when a callback returns false. Previous versions had true.
